fda744667f8e868be033271d9cf746095b855027
JoeMad21 <joeymadden21@gmail.com>
2023-09-04 09:33:36 +0000

    Renamed variables, removed unecessary logic

--- firehose_cpu_genV1/gen_main.cpp
+++ firehose_cpu_genV1/gen_main.cpp
@@ -15,7 +15,7 @@
 
 enum Progs {
   WRITE_INPUTS,
-  REDUCTION_PROG,
+  CONSUMPTION_TASK,
   READ_RESULTS,
   NUM_PROGRAMS
 };
@@ -26,16 +26,16 @@ buildGraphAndPrograms(poplar::Graph &g, const utils::Options &options, long unsi
   using namespace poplar;
 
   // Create some tensor variables. In this case they are just vectors:
-  Tensor v1 = g.addVariable(FLOAT, {dim,dim}, "v1");
-  Tensor v2 = g.addVariable(FLOAT, {dim,dim}, "v2");
-  Tensor v3 = g.addVariable(FLOAT, {dim,dim}, "v3");
+  Tensor in_strm = g.addVariable(FLOAT, {dim,dim}, "in_strm");
+  Tensor proc_mem = g.addVariable(FLOAT, {dim,dim}, "proc_mem");
+  Tensor out_strm = g.addVariable(FLOAT, {dim,dim}, "out_strm");
 
   // Variables need to be explcitly mapped to tiles. Put each variable on
   // a different tile (regardless of whether it is sensible in this case)
 
-  poputil::mapTensorLinearly(g, v1);
-  poputil::mapTensorLinearly(g, v2);
-  poputil::mapTensorLinearly(g, v3);
+  poputil::mapTensorLinearly(g, in_strm);
+  poputil::mapTensorLinearly(g, proc_mem);
+  poputil::mapTensorLinearly(g, out_strm);
 
   // Now can start constructing the programs. Construct a vector of
   // separate programs that can be called individually:
@@ -44,31 +44,26 @@ buildGraphAndPrograms(poplar::Graph &g, const utils::Options &options, long unsi
   // Program that executes all the reduction compute sets:
   auto seq = program::Sequence();
   //seq.add(program::Execute(cs1));
-  progs[REDUCTION_PROG] = seq;
+  progs[CONSUMPTION_TASK] = seq;
 
   // Create streams that allow reading and writing of the variables:
-  auto stream1 = g.addHostToDeviceFIFO("write_x", FLOAT, v1.numElements());
-  auto stream2 = g.addHostToDeviceFIFO("write_y", FLOAT, v2.numElements());
-  auto stream3 = g.addHostToDeviceFIFO("write_z", FLOAT, v3.numElements());
-  auto stream4 = g.addDeviceToHostFIFO("read_z", FLOAT, v3.numElements());
+  auto stream1 = g.addHostToDeviceFIFO("write_x", FLOAT, in_strm.numElements());
+  auto stream2 = g.addHostToDeviceFIFO("write_y", FLOAT, proc_mem.numElements());
+  auto stream3 = g.addHostToDeviceFIFO("write_z", FLOAT, out_strm.numElements());
+  auto stream4 = g.addDeviceToHostFIFO("read_z", FLOAT, out_strm.numElements());
 
   // Add a second compute set that will perform the same calculation using
-  // Poplib's reduction API:
-  //poplar::OptionFlags matmulOptions;
   poplin::addCodelets(g);
-  auto v4 = poplin::matMul(g, v1, v2, seq, FLOAT);
-  seq.add(program::Copy(v4,v3));
-
-  progs[REDUCTION_PROG] = seq;
+  auto mult_out = poplin::matMul(g, in_strm, proc_mem, seq, FLOAT);
+  seq.add(program::Copy(mult_out,out_strm));
 
   // Add program which initialises the inputs. Poplar is able to merge these
   // copies for efficiency:
   progs[WRITE_INPUTS] =
-      program::Sequence({program::Copy(stream1, v1), program::Copy(stream2, v2),
-                         program::Copy(stream3, v3)});
+      program::Sequence({program::Copy(stream1, in_strm), program::Copy(stream2, proc_mem)});
 
   // Add a program to read back the result:
-  progs[READ_RESULTS] = program::Copy(v3, stream4);
+  progs[READ_RESULTS] = program::Copy(out_strm, stream4);
 
   return progs;
 }
@@ -105,7 +100,7 @@ void executeGraphProgram(poplar::Device &device, poplar::Executable &exe,
   // Run program using PopLibs reduction:
   engine.connectStream("read_z", zResult.data());
   engine.run(WRITE_INPUTS);
-  engine.run(REDUCTION_PROG);
+  engine.run(CONSUMPTION_TASK);
   engine.run(READ_RESULTS);
 
   std::cout << "Matrix 1\n";

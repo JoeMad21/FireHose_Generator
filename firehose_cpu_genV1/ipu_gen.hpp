10a9d9b13031afb970c0d64c240fc361a284e301
JoeMad21 <joeymadden21@gmail.com>
2023-09-05 16:22:00 +0000

    Something wrong with separation

---
+++ firehose_cpu_genV1/ipu_gen.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdlib>
+#include <vector>
+#include <random>
+#include <iomanip>
+#include "utils.h"
+
+#include <poputil/TileMapping.hpp>
+
+#include <poplin/codelets.hpp>
+#include <poplin/MatMul.hpp>
+
+
+std::vector<poplar::program::Program>
+buildGraphAndPrograms(poplar::Graph &g, const utils::Options &options, long unsigned int dim);
+
+void executeGraphProgram(poplar::Device &device, poplar::Executable &exe,
+                         const utils::Options &options, long unsigned int dim);
+
+void launchOnIPU(long unsigned matrix_dim, int argc, char **argv);

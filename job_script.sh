5ef8f9c6f8e16da019f5c057e2611d54815b6648
JoeMad21 <joeymadden21@gmail.com>
2023-11-02 22:18:55 +0000

    Fixed a matrix dimension issue

--- job_script.sh
+++ job_script.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-./gen_demo --device 1 --con_task 1 --source 1 --dimension 4
+#SBATCH --job-name FireHose_Generator
+#SBATCH --ipus=1
+--partition=p64
+#SBATCH --nodelist=gc-poplar-03
+#SBATCH --ntasks 1
+#SBATCH --time=00:05:00
+
+srun ./gen_demo --device 1 --con_task 1 --source 1 --dimension 6
